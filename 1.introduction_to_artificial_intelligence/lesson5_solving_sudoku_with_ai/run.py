56b04392b1235c97702b88c32868dcf23fb97316
HanByul Yang <hanbyul.yang@gmail.com>
2020-07-18 14:24:03 +0900

    10. Coding the Solution

--- 1.introduction_to_artificial_intelligence/lesson5_solving_sudoku_with_ai/run.py
+++ 1.introduction_to_artificial_intelligence/lesson5_solving_sudoku_with_ai/run.py
@@ -1,10 +1,9 @@
-from function import *
-
-from utils import display
+from function import search
+from utils import *
 
 grid1 = '..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..'
 # display(reduce_puzzle(grid_values(grid1)))
 
 grid2 = '4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......'
 values = grid_values(grid2)
-display(reduce_puzzle(values))
+display(search(values))
