faafec7aabc4777c7e1cc2ad1471a186198cf391
HanByul Yang <hanbyul.yang@gmail.com>
2020-07-18 14:08:19 +0900

    7. Constraint Propagation

--- 1.introduction_to_artificial_intelligence/lesson5_solving_sudoku_with_ai/run.py
+++ 1.introduction_to_artificial_intelligence/lesson5_solving_sudoku_with_ai/run.py
@@ -4,4 +4,4 @@ from utils import display
 
 input_string = '..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..'
 
-display(only_choice(eliminate(grid_values(input_string))))
+display(reduce_puzzle(grid_values(input_string)))
