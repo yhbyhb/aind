1e07a293010c1c63ed59d0442e542fbec6b26b95
HanByul Yang <hanbyul.yang@gmail.com>
2020-08-17 18:30:12 +0900

    Lesson 1. Search In Multiagent Domains.   - 13. Coding: Scoring Min & Max Levels

--- 6.adversarial_search/lesson1_search_in_multiagent_domains/gamestate.py
+++ 6.adversarial_search/lesson1_search_in_multiagent_domains/gamestate.py
@@ -1,6 +1,9 @@
+
+# Please use this implementation for compatability with the test cases
+
 from copy import deepcopy
 
-xlim, ylim = 3, 2  # board dimension constants
+xlim, ylim = 3, 2  # board dimensions
 
 # The eight movement directions possible for a chess queen
 RAYS = [(1, 0), (1, -1), (0, -1), (-1, -1),
@@ -11,43 +14,39 @@ class GameState:
     """
     Attributes
     ----------
-    TODO: Copy in your implementation from the previous quiz
+    _board: list(list)
+        Represent the board with a 2d array _board[x][y]
+        where open spaces are 0 and closed spaces are 1
+    
+    _parity: bool
+        Keep track of active player initiative (which
+        player has control to move) where 0 indicates that
+        player one has initiative and 1 indicates player 2
+    
+    _player_locations: list(tuple)
+        Keep track of the current location of each player
+        on the board where position is encoded by the
+        board indices of their last move, e.g., [(0, 0), (1, 0)]
+        means player 1 is at (0, 0) and player 2 is at (1, 0)
     """
     def __init__(self):
-        # TODO: Copy in your implementation from the previous quiz
         self._board = [[0] * ylim for _ in range(xlim)]
         self._board[-1][-1] = 1  # block lower-right corner
         self._parity = 0
         self._player_locations = [None, None]
         
     def actions(self):
-        """ Return a list of legal actions for the active player 
-        
-        You are free to choose any convention to represent actions,
-        but one option is to represent actions by the (row, column)
-        of the endpoint for the token. For example, if your token is
-        in (0, 0), and your opponent is in (1, 0) then the legal
-        actions could be encoded as (0, 1) and (0, 2).
-        """
-        # TODO: Finish this function!
+        """ Return a list of legal actions for the active player """
         return self.liberties(self._player_locations[self._parity])
     
     def player(self):
-        """ Return the id of the active player 
-        
-        Hint: return 0 for the first player, and 1 for the second player
-        """
-        # TODO: Finish this function!
+        """ Return the id of the active player """
         return self._parity
     
     def result(self, action):
         """ Return a new state that results from applying the given
         action in the current state
-        
-        Hint: Check out the deepcopy module--do NOT modify the
-        objects internal state in place
         """
-        # TODO: Finish this function!
         assert action in self.actions(), "Attempted forecast of illegal move"
         newBoard = deepcopy(self)
         newBoard._board[action[0]][action[1]] = 1
@@ -63,9 +62,20 @@ class GameState:
         player has no remaining liberties (even if the
         player is not active in the current state)
         """
-        # TODO: Finish this function!
         return (not self._has_liberties(self._parity)
             or not self._has_liberties(1 - self._parity))
+
+    def utility(self, player_id):
+        """ return +inf if the game is terminal and the
+        specified player wins, return -inf if the game
+        is terminal and the specified player loses, and
+        return 0 if the game is not terminal
+        """
+        if not self.terminal_test(): return 0
+        player_id_is_active = (player_id == self.player())
+        active_has_liberties = self._has_liberties(self.player())
+        active_player_wins = (active_has_liberties == player_id_is_active)
+        return float("inf") if active_player_wins else float("-inf")
     
     def liberties(self, loc):
         """ Return a list of all open cells in the
@@ -74,11 +84,7 @@ class GameState:
         along any row, column or diagonal from the current
         position. (Tokens CANNOT move through obstacles
         or blocked squares in queens Isolation.)
-        
-        Note: if loc is None, then return all empty cells
-        on the board
         """
-        # TODO: Finish this function!
         if loc is None: return self._get_blank_spaces()
         moves = []
         for dx, dy in RAYS:  # check each movement direction
