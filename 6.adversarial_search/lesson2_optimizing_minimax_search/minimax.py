5d689eb3229bdf2b87dd1ad1a786313757626967
HanByul Yang <hanbyul.yang@gmail.com>
2020-08-27 19:11:14 +0900

    Lesson 2: Optimizing Minimax Search 25. Coding: Alpha-Beta Pruning

--- 6.adversarial_search/lesson2_optimizing_minimax_search/minimax.py
+++ 6.adversarial_search/lesson2_optimizing_minimax_search/minimax.py
@@ -1,13 +1,5 @@
 
-# DO NOT MODIFY THE PLAYER ID
-player_id = 0
-
-def my_moves(gameState):
-    loc = gameState._player_locations[player_id]
-    return len(gameState.liberties(loc))
-
-
-def minimax_decision(gameState, depth):
+def alpha_beta_search(gameState):
     """ Return the move along a branch of the game tree that
     has the best possible value.  A move is a pair of coordinates
     in (column, row) order corresponding to a legal move for
@@ -16,18 +8,20 @@ def minimax_decision(gameState, depth):
     You can ignore the special case of calling this function
     from a terminal state.
     """
+    alpha = float("-inf")
+    beta = float("inf")
     best_score = float("-inf")
     best_move = None
     for a in gameState.actions():
-        # call has been updated with a depth limit
-        v = min_value(gameState.result(a), depth - 1)
+        v = min_value(gameState.result(a), alpha, beta)
+        alpha = max(alpha, v)
         if v > best_score:
             best_score = v
             best_move = a
     return best_move
 
-
-def min_value(gameState, depth):
+# TODO: modify the function signature to accept an alpha and beta parameter
+def min_value(gameState, alpha, beta):
     """ Return the value for a win (+1) if the game is over,
     otherwise return the minimum value over all legal child
     nodes.
@@ -35,17 +29,18 @@ def min_value(gameState, depth):
     if gameState.terminal_test():
         return gameState.utility(0)
     
-    if depth <= 0:
-        return my_moves(gameState)
-    
     v = float("inf")
     for a in gameState.actions():
-        # the depth should be decremented by 1 on each call
-        v = min(v, max_value(gameState.result(a), depth - 1))
+        # TODO: modify the call to max_value()
+        v = min(v, max_value(gameState.result(a), alpha, beta))
+        # TODO: update the value bound
+        if v <= alpha: 
+            return v
+        beta = min(beta, v)
     return v
 
-
-def max_value(gameState, depth):
+# TODO: modify the function signature to accept an alpha and beta parameter
+def max_value(gameState, alpha, beta):
     """ Return the value for a loss (-1) if the game is over,
     otherwise return the maximum value over all legal child
     nodes.
@@ -53,11 +48,12 @@ def max_value(gameState, depth):
     if gameState.terminal_test():
         return gameState.utility(0)
     
-    if depth <= 0:
-        return my_moves(gameState)
-    
     v = float("-inf")
     for a in gameState.actions():
-        # the depth should be decremented by 1 on each call
-        v = max(v, min_value(gameState.result(a), depth - 1))
+        # TODO: modify the call to min_value()
+        v = max(v, min_value(gameState.result(a), alpha, beta))
+        # TODO: update the value bound
+        if v >= beta: 
+            return v
+        alpha = max(alpha, v)
     return v
--- 6.adversarial_search/lesson2_optimizing_minimax_search/testcode.py
+++ 6.adversarial_search/lesson2_optimizing_minimax_search/testcode.py
@@ -1,21 +1,20 @@
 
-import search
+import minimax
 import gamestate as game
 
 
 # Test the depth limit by checking the number of nodes visited
 # -- recall that minimax visits every node in the search tree,
 # so if we search depth one on an empty board then minimax should
-# visit the sum of each sub-tree
-depth_limit = 2
-expected_node_count = 30
+# visit each of the five open spaces
+expected_node_count = 55
 rootNode = game.GameState()
-search.get_action(rootNode, depth_limit)
+minimax.alpha_beta_search(rootNode)
 
 print("Expected node count: {}".format(expected_node_count))
 print("Your node count: {}".format(game.call_counter))
 
 if game.call_counter == expected_node_count:
-    print("That's right! Looks like your depth limit is working!")
+    print("That's right! Looks like your alpha-beta pruning is working!")
 else:
     print("Uh oh...looks like there may be a problem.")
